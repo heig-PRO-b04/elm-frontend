d048bc9e8c7d4529f3960d00651c735668dff146
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-03-02 13:03:07 +0100

    Change some build configuration and add buttons
    Custom styling can now be defined through Tailwind CSS directly, that
    will generate appropriate colors and more to be used in the application
    through utility classes.
    
    Buttons with composable styling are also now available.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, a, button, div, img, p, text)
-import Html.Attributes exposing (class, disabled, src)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Picasso.Button exposing (button, elevated, outlined)
 
 
 type alias Counter =
@@ -17,7 +18,7 @@ type Message
 
 init : () -> ( Counter, Cmd Message )
 init _ =
-    ( 42, Cmd.none )
+    ( 0, Cmd.none )
 
 
 subscriptions : Counter -> Sub Message
@@ -39,65 +40,16 @@ update msg counter =
     ( value, Cmd.none )
 
 
-placeholder : Counter -> Html Message
-placeholder counter =
-    let
-        title =
-            "Finding " ++ String.fromInt counter ++ " ideas for our project"
-    in
-    div
-        [ class "md:flex"
-        , class "rounded"
-        , class "overflow-hidden"
-        , class "shadow-lg"
-        , class "container"
-        ]
-        [ div [ class "md:flex-shrink-0" ]
-            [ img
-                [ class "md:w-56"
-                , src "https://heig-vd.ch/images/default-source/img-global-superpicture/default/heig-vd-site-web-lg-00081917--lg.jpg?sfvrsn=521582ea_2"
-                ]
-                []
-            ]
-        , div
-            [ class "mt-4"
-            , class "md:mt-0"
-            , class "md:ml-6"
-            ]
-            [ div
-                [ class "uppercase"
-                , class "tracking-wide"
-                , class "text-sm"
-                , class "text-red-600"
-                , class "font-bold"
-                ]
-                [ text "Marketing" ]
-            , a
-                [ class "block"
-                , class "mt-1"
-                , class "text-lg"
-                , class "leading-tight"
-                , class "font-semibold"
-                , class "text-gray-900"
-                , class "hover:underline"
-                , onClick Increment
-                ]
-                [ text title ]
-            , p [ class "mt-2", class "text-gray-600" ]
-                [ text """
-                This is just a sample card that we might want
-                to use in the project. We'll see actually. By
-                the way, pressing on the link will increment the
-                counter value, and will therefore update the web
-                page.
-                """ ]
-            ]
-        ]
-
-
 view : Counter -> Html Message
 view counter =
-    placeholder counter
+    div
+        [ class "h-screen w-screen"
+        , class "flex flex-col items-center justify-center"
+        ]
+        [ button
+            (elevated ++ outlined ++ [ onClick Increment ])
+            [ text <| String.fromInt counter ]
+        ]
 
 
 main : Program () Counter Message
