7aefe885b00d382ee8ea79d66b081643dc7f2948
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-09 13:13:24 +0200

    Remove the navigation UI import

--- src/Page/Authenticate.elm
+++ src/Page/Authenticate.elm
@@ -26,7 +26,6 @@ import Html.Attributes exposing (class, placeholder, type_)
 import Html.Events exposing (onClick, onInput)
 import Picasso.Button exposing (button, elevated, filled, filledDisabled, outlinedLight)
 import Picasso.Input as Input
-import Picasso.Navigation as Navigation
 import Picasso.Text exposing (styledH2)
 import Route
 import Session exposing (Session)
