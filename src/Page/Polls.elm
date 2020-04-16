1c7829c0f0dd3568bed95ffb6fab0c38f55f1ab1
matt989253 <matthieu.burguburu@gmail.com>
2020-04-16 19:10:24 +0200

    Add a mock polls page

--- src/Page/Polls.elm
+++ src/Page/Polls.elm
@@ -7,7 +7,10 @@ module Page.Polls exposing
     )
 
 import Cmd exposing (withNoCmd)
-import Html exposing (Html, text)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+import Picasso.Button exposing (button, filled, outlined)
+import Picasso.Text exposing (styledH2)
 import Session exposing (Session)
 
 
@@ -33,4 +36,61 @@ update _ model =
 
 view : Model -> List (Html Message)
 view model =
-    [ text "Hello from the Polls page" ]
+    [ div
+        [ class "flex flex-col" ]
+        [ div
+            [ class "flex flex-row justify-center" ]
+            [ actionButton "Create a new poll"
+            , actionButton "See your archived polls"
+            ]
+        , div [ class "px-3 pb-3" ] [ styledH2 "Your polls:" ]
+        , pollButton "This is a poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        , pollButton "This is another poll title"
+        ]
+    ]
+
+
+actionButton content =
+    button
+        (filled
+            ++ [ class "m-4"
+               ]
+        )
+        [ text content ]
+
+
+pollButton content =
+    div
+        [ class "flex flex-row" ]
+        [ button
+            (outlined
+                ++ [ class "flex-grow"
+                   , class "mb-2 mx-2"
+                   ]
+            )
+            [ text content ]
+        , button
+            (filled
+                ++ [ class "flex-shrink"
+                   , class "mb-2 mr-2"
+                   ]
+            )
+            [ text "arch" ]
+        , button
+            (filled
+                ++ [ class "flex-shrink"
+                   , class "mb-2 mr-2"
+                   ]
+            )
+            [ text "del" ]
+        ]
