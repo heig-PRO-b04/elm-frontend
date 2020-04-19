c2c520171752d38c8e3b714dc4d5336855002843
Alexandre Piveteau <alexandre@piveteau.email>
2020-04-19 12:28:34 +0200

    Persist authentication credentials in local storage

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 import { Elm } from "./src/Main.elm";
 
 // Maybe we will want to pass the API endpoint in the program flags.
+const storageKey = "credentials";
+
+const flags = localStorage.getItem(storageKey);
 const app = Elm.Main.init({
-  node: document.querySelector("main")
+  node: document.querySelector("main"),
+  flags: flags
+});
+
+
+
+app.ports.portStoreCredentials.subscribe(function (val) {
+  if (val == null) {
+    localStorage.removeItem(storageKey);
+  } else {
+    localStorage.setItem(storageKey, JSON.stringify(val));
+  }
+
+  setTimeout(function () {
+    app.ports.portSubscribeCredentials.send(val);
+  }, 0);
+});
+
+window.addEventListener("storage", function (event) {
+  if (event.storageArea === localStorage && event.key === storageKey) {
+    app.ports.portSubscribeCredentials.send(event.newValue);
+  }
 });
