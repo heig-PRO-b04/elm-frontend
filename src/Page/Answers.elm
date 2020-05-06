2419e0220fff1fcca3b5472d083839cee2126ba7
matt989253 <matthieu.burguburu@gmail.com>
2020-05-06 02:15:23 +0200

    Having trouble with transferring Messages from Answers to QuestionList

--- src/Page/Answers.elm
+++ src/Page/Answers.elm
@@ -2,19 +2,19 @@ module Page.Answers exposing
     ( Message
     , Model
     , init
-    , show
     , update
     , view
     )
 
-import Api.Answers exposing (ServerAnswer)
+import Api.Answers exposing (ClientAnswer, ServerAnswer)
 import Api.Questions exposing (QuestionDiscriminator, QuestionVisibility(..), ServerQuestion)
 import Cmd exposing (withCmd, withNoCmd)
-import Html exposing (Html, div)
-import Html.Attributes exposing (class)
-import Html.Events as Html
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class, placeholder, value)
+import Html.Events exposing (onClick, onInput)
+import Picasso.Input as Input
 import Session exposing (Viewer)
-import Task
+import Task exposing (Task)
 import Task.Extra
 
 
@@ -31,19 +31,20 @@ type AnswersState
 type alias Model =
     { viewer : Viewer
     , state : AnswersState
+    , question : QuestionDiscriminator
+    , titleInput : String
     }
 
 
 init : Viewer -> { d | idPoll : Int, idQuestion : Int } -> ( Model, Cmd Message )
 init viewer discriminator =
-    { viewer = viewer
-    , state = Loading
-    }
-        |> withCmd
-            [ QuestionDiscriminator discriminator.idPoll discriminator.idQuestion
-                |> NowRequestAnswers
-                |> Cmd.succeed
-            ]
+    ( { viewer = viewer
+      , state = Loading
+      , question = QuestionDiscriminator discriminator.idPoll discriminator.idQuestion
+      , titleInput = "placeholder text"
+      }
+    , Cmd.succeed NowRequestAnswers
+    )
 
 
 
@@ -51,23 +52,25 @@ init viewer discriminator =
 
 
 type Message
-    = NowRequestAnswers QuestionDiscriminator
+    = NowRequestAnswers
     | GotAnswerList (List ServerAnswer)
+    | NowCreateAnswer ClientAnswer
     | GotInvalidCredentials
     | GotError
+    | WriteNewTitle String
 
 
 update : Message -> Model -> ( Model, Cmd Message )
 update msg model =
     case msg of
-        NowRequestAnswers questionDiscriminator ->
+        NowRequestAnswers ->
             let
                 viewer =
                     Session.viewerCredentials model.viewer
             in
             model
                 |> withCmd
-                    [ Api.Answers.getAnswerList viewer questionDiscriminator identity
+                    [ Api.Answers.getAnswerList viewer model.question identity
                         |> Task.map GotAnswerList
                         |> Task.mapError
                             (\error ->
@@ -93,24 +96,70 @@ update msg model =
             { model | state = Error model.viewer }
                 |> withNoCmd
 
+        WriteNewTitle string ->
+            { model | titleInput = string }
+                |> withNoCmd
+
+        NowCreateAnswer clientAnswer ->
+            let
+                viewer =
+                    Session.viewerCredentials model.viewer
+            in
+            model
+                |> withCmd
+                    [ Api.Answers.create viewer model.question clientAnswer identity
+                        |> Task.mapError
+                            (\error ->
+                                case error of
+                                    Api.Answers.GotBadCredentials ->
+                                        GotInvalidCredentials
+
+                                    _ ->
+                                        GotError
+                            )
+                        |> Task.andThen (always <| Task.succeed NowRequestAnswers)
+                        |> Task.Extra.execute
+                    ]
+
 
 
 -- VIEW
 
 
-show : Html msg
-show =
-    div [] [ Html.text "show" ]
-
-
-view : Model -> Html msg
+view : Model -> Html Message
 view model =
+    div
+        []
+        [ Html.input [ value model.titleInput, onInput WriteNewTitle ] []
+        , Html.button [ onClick <| NowCreateAnswer <| ClientAnswer model.titleInput "" ] [ Html.text "**save**" ]
+        ]
+
+
+inputAnswerTitle : Model -> Html Message
+inputAnswerTitle model =
+    div []
+        [ Input.inputWithTitle "Answer title: "
+            [ onInput WriteNewTitle
+            , placeholder "placeholder"
+            , value model.titleInput
+            ]
+            []
+        ]
+
+
+showAnswerList : Model -> Html msg
+showAnswerList model =
     case model.state of
         Loading ->
             div [] [ Html.text "Loading" ]
 
         Loaded serverAnswers ->
-            div [] [ Html.text "Loaded" ]
+            div [] (List.map (\answer -> showAnswer answer) serverAnswers)
 
         Error viewer ->
             div [] [ Html.text "Error" ]
+
+
+showAnswer : ServerAnswer -> Html msg
+showAnswer answer =
+    Html.text answer.title
