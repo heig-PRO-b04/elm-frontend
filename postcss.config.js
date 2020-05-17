aa48801973f6e73786c2100a5e25520ea8354c0a
Alexandre Piveteau <alexandre@piveteau.email>
2020-05-17 22:27:48 +0200

    Revert "Use purge-css in production"
    This reverts commit 4fc033c10961e2aba6e59efc95b69d25e968fc14.

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require("autoprefixer"),
-    require('postcss-purgecss')({
-      content: [
-          './src/**/*.elm',
-          './index.html',
-          './static/404.html'
-      ],
-      defaultExtractor: content => content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-    })
+    require("autoprefixer")
   ]
 };
